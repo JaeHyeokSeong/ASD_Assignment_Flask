eef5418276a67204d8736333e7f42d77f3204ed7
Pascal Terwesten <pascal@Pascals-MacBook-Air-3.local>
2023-09-27 19:07:50 +1000

    try again

--- user_handling_test.py
+++ user_handling_test.py
@@ -5,6 +5,14 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
+        class MyTestCase(unittest.TestCase):
+            def test_something(self):
+                self.assertEqual(True, True)  # add assertion here
+
+                class MyTestCase(unittest.TestCase):
+                    def test_something(self):
+                        self.assertEqual(True, True)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
