0624f4a355962726929179e2a59ba55b1fdb633a
Pascal Terwesten <pascal@192-168-1-115.tpgi.com.au>
2023-09-28 14:50:04 +1000

    try again

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/baseAgent')
+def base_agent():
+    return render_template('baseAgent.html')
 
-app.run(debug=True)
+@app.route('/welcomeAgent')
+def welcome_agent():
+    return render_template('welcomeAgent.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
