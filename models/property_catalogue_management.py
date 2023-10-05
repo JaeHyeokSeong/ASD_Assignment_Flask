dae79d35849eb5a07321e51b375127716531cbf3
JaeHyeokSeong <seong5763@gmail.com>
2023-10-05 20:33:21 +1100

    create property catalogue model

---
+++ models/property_catalogue_management.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import random
+import string
+
+
+class PropertyCatalogue:
+
+    def __init__(self, mycursor, mydb):
+        self.mycursor = mycursor
+        self.mydb = mydb
+
+    def add_property(self):
+        pass
+
+    def find_property(self):
+        pass
+
+    def update_property(self):
+        pass
+
+    def delete_property(self):
+        pass
+
