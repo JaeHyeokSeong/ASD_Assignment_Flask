e03d6c602f5f86178ae74fb6235060f00c33b380
JaeHyeokSeong <seong5763@gmail.com>
2023-10-07 06:38:35 +1100

    unittest practise

--- property_catalogue_test.py
+++ property_catalogue_test.py
@@ -1,3 +1,5 @@
+# https://www.daleseo.com/python-unittest-testcase/ [how to write unittest]
+# https://wikidocs.net/16107 [how to write unittest]
 import unittest
 from models.property_catalogue_management import PropertyCatalogue
 
@@ -5,9 +7,13 @@ from models.property_catalogue_management import PropertyCatalogue
 class PropertyCatalogues(unittest.TestCase):
 
     def setUp(self) -> None:
+        print('test has started')
         pc = PropertyCatalogue()
         result = pc.search_property_by_property_id_agent(1, 2)
         self.result = result[0]
+
+    def tearDown(self) -> None:
+        print('test has finished')
 
     def test_find_property(self):
         self.assertEqual(self.result['property_id'], 1)
