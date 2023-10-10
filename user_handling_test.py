91a1bad220f8428921f3bfddb193dd2c94df66ad
Pascal Terwesten <pascal@Pascals-iMac.local>
2023-10-10 16:52:26 +1100

    REMOTE DATABASE

--- user_handling_test.py
+++ user_handling_test.py
@@ -1,54 +1,62 @@
 import unittest
-import mysql.connector
-import random
-import string
+from unittest.mock import Mock
+
+# Assuming your UserManagement class is in a file named user_management.py
 from models.user_management import UserManagement
 
-class MyTestCase(unittest.TestCase):
+class TestUserManagement(unittest.TestCase):
+
     def setUp(self):
-        # Connect to the testing database
-        self.test_db = mysql.connector.connect(
-            host="asd-spring2023.mysql.database.azure.com",
-            user="pascal",
-            password="asd2023Group3",
-            port='3306',
-            database='python_db',
-            ssl_ca='/var/www/html/DigiCertGlobalRootG2.crt.pem'
-        )
-
-        # Create a cursor from the testing database connection
-        self.test_cursor = self.test_db.cursor()
-
-        # Generate a unique random ID for each test run
-        self.random_id = random.randint(100000, 999999)
-
-        # Create an instance of UserManagement with the testing database cursor and connection
-        self.user_management = UserManagement(self.test_cursor, self.test_db)
-
-    def tearDown(self):
-        # Delete the test user from the database after each test
-        self.test_cursor.execute("DELETE FROM users WHERE id = %s", (self.random_id,))
-        # Commit the changes to the database
-        self.test_db.commit()
-        # Close the database connection after each test
-        self.test_db.close()
+        # Create mock database and cursor for testing
+        self.mock_db = Mock()
+        self.mock_cursor = Mock()
+        self.user_management = UserManagement(self.mock_cursor, self.mock_db)
 
     def test_add_user_to_database(self):
-        name = "John Doe"
-        email = "johndoe@example.com"
-        user_type = "agent"
-        password = "password123"
-        phone = "123-456-7890"
+        # Define test data
+        test_data = ("random123", "John", "john@example.com", "admin", "password123", "1234567890")
+
+        # Call the method with test data
+        self.user_management.add_user_to_database(*test_data)
+
+        # Ensure the correct SQL is called with the right parameters
+        self.mock_cursor.execute.assert_called_with(
+            "INSERT INTO users (id, name, email, userType, password, phone) VALUES (%s, %s, %s, %s, %s, %s)",
+            test_data
+        )
+        # Ensure commit is called on the database
+        self.mock_db.commit.assert_called()
+
+    def test_get_user_info_from_database(self):
+        # Mocking the fetchone return value
+        self.mock_cursor.fetchone.return_value = ("John", "john@example.com", "1234567890", "password123")
 
         # Call the method
-        self.user_management.add_user_to_database(self.random_id, name, email, user_type, password, phone)
+        result = self.user_management.get_user_info_from_database("random123")
 
-        # Retrieve the user data from the database for assertions
-        self.test_cursor.execute("SELECT name, email, phone, password FROM users WHERE id = %s", (self.random_id,))
-        user_data = self.test_cursor.fetchone()
+        # Ensure we got the expected result
+        self.assertEqual(result, ("John", "john@example.com", "1234567890", "password123"))
 
-        # Assert that the user data in the database matches the input data
-        self.assertEqual(user_data, (name, email, phone, password))
+    def test_authenticate_user_success(self):
+        # Mock a successful authentication
+        self.mock_cursor.fetchone.return_value = ("random123",)
 
-if __name__ == '__main__':
+        # Test method
+        result = self.user_management.authenticate_user("john@example.com", "password123", "admin")
+
+        # Ensure the correct ID is returned
+        self.assertEqual(result, "random123")
+
+    def test_authenticate_user_fail(self):
+        # Mock a failed authentication
+        self.mock_cursor.fetchone.return_value = None
+
+        # Test method
+        result = self.user_management.authenticate_user("john@example.com", "wrong_password", "admin")
+
+        # Ensure None is returned
+        self.assertIsNone(result)
+
+
+if __name__ == "__main__":
     unittest.main()
