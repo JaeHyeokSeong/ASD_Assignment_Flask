8e4da86c6eb59d60095c51ab3b6e6fbb0f8e24ba
Migyeongjeong <Migyeong.jeong-1@student.uts.edu.au>
2023-10-12 17:01:52 +1100

    New Changes

--- models/landlord_management.py
+++ models/landlord_management.py
@@ -12,7 +12,7 @@ class LandlordManagement:
     def get_property_income(self, property_id):
         self.mycursor.execute("SELECT * FROM Income WHERE property_id = %s", (property_id,))
 
-        column_names = [desc[0] for desc in self.mycursor.description]  # Get column names
+        column_names = [desc[0] for desc in self.mycursor.description]
         results = self.mycursor.fetchall()
 
         income = [dict(zip(column_names, row)) for row in results]
