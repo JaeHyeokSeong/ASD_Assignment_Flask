ccc52a2ff13bf2610473cbfed16cd6addd9fd973
Pascal <126212787+iampascal@users.noreply.github.com>
2023-10-13 12:42:12 +1100

    Hoon Features

---
+++ property_inspection_test.py
@@ -0,0 +1,39 @@
+import unittest
+from unittest.mock import Mock
+from models.propertyInspection import propertyInspection
+from HTMLTestRunner import HTMLTestRunner
+
+class TestPropertyInspection(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_db = Mock()
+        self.mock_cursor = Mock()
+        self.property_inspection = propertyInspection(self.mock_cursor, self.mock_db)
+
+    def test_add_inspection_to_database(self):
+        inspection_id = 1
+        property_id = 1  # Replace with your property_id
+        agent_id = 2  # Replace with your agent_id
+        tenant_id = 3  # Replace with your tenant_id
+        inspection_date = "2023-10-12"  # Replace with your inspection_date
+
+        self.property_inspection.add_inspection_to_database(inspection_id, property_id, agent_id, tenant_id, inspection_date)
+
+        self.mock_cursor.execute.assert_called_with(
+            "INSERT INTO inspection (inspection_id ,property_id, agent_id, tenant_id, inspection_date) VALUES (%s, %s, %s, %s, %s)",
+            (1, 1, 2, 3, "2023-10-12")
+        )
+        self.mock_db.commit.assert_called()
+
+    def test_get_inspection_info_from_database(self):
+        agent_id = 2  # Replace with your agent_id
+        self.mock_cursor.fetchall.return_value = [("123", 1, 2, 3, "2023-10-12")]
+        result = self.property_inspection.get_inspection_info_from_database(agent_id)
+        self.assertEqual(result, [("123", 1, 2, 3, "2023-10-12")])
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPropertyInspection)
+    with open('unittest_results/test_property_inspection_result.html', 'w') as f:
+        runner = HTMLTestRunner.HTMLTestRunner(stream=f, title='Unit Test Report for Property Inspection')
+        runner.run(suite)
+    unittest.main()
